151b77f1cc23938fa88cfd1274925365c1e6e0f5
marti.westermeyer <marti.westermeyer@estudiantat.upc.edu>
2021-03-18 16:51:25 +0100

    arreglo

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,4 +1,4 @@
-const google_logo = 'images/google_login.png';
+const google_logo = 'images/google_logo.png';
 const facebook_logo = 'images/facebook_logo.png';
 const socialcraft_logo = 'images/socialcraft_logo.png';
 const socialcraft_logo_letras = 'images/socialcraft_logo_letras.png';
