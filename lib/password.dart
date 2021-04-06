55a4c204e27d2bff6e61d4daeca2ca6a7ac8a836
iamfelixf <felix.forroll@gmail.com>
2021-04-06 16:48:16 +0200

    cagada solucionada

--- lib/password.dart
+++ lib/password.dart
@@ -6,6 +6,7 @@ import 'package:socialcraft/utils/fonts.dart';
 import 'package:socialcraft/utils/images.dart';
 import 'package:socialcraft/resp.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class Password extends StatefulWidget {
   static String tag = '/password';
@@ -21,7 +22,10 @@ class PasswordState extends State<Password> {
     init();
   }
 
-  init() async {}
+  init() async {
+    final storage2 = new FlutterSecureStorage();
+    token = await storage2.read(key: 'jwt');
+  }
 
   @override
   void setState(fn) {
@@ -31,9 +35,10 @@ class PasswordState extends State<Password> {
   String passActual = '';
   String passNew = '';
   String passNewConfirm = '';
-
+  String token = "";
+/*
   String token =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpLnNvY2lhbGNyYWZ0LmNsdWIiLCJpYXQiOjE2MTY2NjY0MjMsInVzZXJJZCI6IjEyIn0.zSfDG1InrzwC9dQWwYbinGyqW27DzpNNnr9bHZw_AvhfKFDPLXeR4Gf6JNw9FhsrmyzyRg0Z5TtngROGglRee8fAIUBAndnNCj10RR6R-TWs71SkZa_3-NKK4Y8LWtNBTJbjgOx_9IGRyL7TmAyliHNBnA7WRImwmF9gLbH0ay-s64VY7y70BW3ez0iasaJrzDTEGJqOcdhWo7eq-3F1fgSOTtW2TGfT-6zOCA7klSPwHdiddrdhmRS5nrXme3tZ-Hb34Lhy7He-Bgg10PFPxS2J7CtVTNR_heUxzXw3TSObtcSqYTiHRmVoJfP4UaDmbWTa7A96-TpjnnZZwj3kYg';
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvYXBpLnNvY2lhbGNyYWZ0LmNsdWIiLCJpYXQiOjE2MTY2NjY0MjMsInVzZXJJZCI6IjEyIn0.zSfDG1InrzwC9dQWwYbinGyqW27DzpNNnr9bHZw_AvhfKFDPLXeR4Gf6JNw9FhsrmyzyRg0Z5TtngROGglRee8fAIUBAndnNCj10RR6R-TWs71SkZa_3-NKK4Y8LWtNBTJbjgOx_9IGRyL7TmAyliHNBnA7WRImwmF9gLbH0ay-s64VY7y70BW3ez0iasaJrzDTEGJqOcdhWo7eq-3F1fgSOTtW2TGfT-6zOCA7klSPwHdiddrdhmRS5nrXme3tZ-Hb34Lhy7He-Bgg10PFPxS2J7CtVTNR_heUxzXw3TSObtcSqYTiHRmVoJfP4UaDmbWTa7A96-TpjnnZZwj3kYg'; */
 
   Future<Resp> cambiarpassword(String p, String p2) async {
     var map = new Map<String, dynamic>();
