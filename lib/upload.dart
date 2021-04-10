8e31c9820d9857aaf6ebf4c60fabd049d126db5d
iamfelixf <felix.forroll@gmail.com>
2021-04-10 17:59:48 +0200

    time pick

--- lib/upload.dart
+++ lib/upload.dart
@@ -8,6 +8,7 @@ import 'package:socialcraft/utils/fonts.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:smart_select/smart_select.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
 
 class Upload extends StatefulWidget {
   static String tag = '/settings';
@@ -92,14 +93,7 @@ class UploadState extends State<Upload> {
               )
                   .cornerRadiusWithClipRRect(12)
                   .paddingOnly(top: 30, left: 30, right: 30),
-              30.height,
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'TIEMPO ESTIMADO',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ).paddingOnly(left: 30),
-              ),
+              10.height,
               SmartSelect<String>.single(
                 modalConfig: S2ModalConfig(
                   type: S2ModalType.popupDialog,
@@ -119,38 +113,40 @@ class UploadState extends State<Upload> {
               ),
               SmartSelect<String>.single(
                 modalConfig: S2ModalConfig(
-                  type: S2ModalType.popupDialog,
-                  style: S2ModalStyle(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  type: S2ModalType.fullPage,
                 ),
                 modalHeaderStyle: S2ModalHeaderStyle(
                     backgroundColor: azul_logo,
                     textStyle: TextStyle(color: white)),
                 title: 'Categoria',
-                value: value1,
-                choiceItems: options,
-                onChange: (state) => setState(() => value1 = state.value),
+                value: value2,
+                choiceItems: categorias,
+                onChange: (state) => setState(() => value2 = state.value),
               ),
               SmartSelect<int>.multiple(
-                modalConfig: S2ModalConfig(
-                  type: S2ModalType.popupDialog,
-                  style: S2ModalStyle(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+                  modalConfig: S2ModalConfig(
+                    type: S2ModalType.fullPage,
                   ),
-                ),
-                modalHeaderStyle: S2ModalHeaderStyle(
-                    backgroundColor: azul_logo,
-                    textStyle: TextStyle(color: white)),
-                title: 'Materiales',
-                value: value3,
-                choiceItems: frameworks,
-                onChange: (state) => setState(() => value3 = state.value),
-              ),
+                  //choiceLayout: S2ChoiceLayout.list,
+                  modalHeaderStyle: S2ModalHeaderStyle(
+                      backgroundColor: azul_logo,
+                      textStyle: TextStyle(color: white)),
+                  title: 'Materiales',
+                  value: value3,
+                  choiceItems: frameworks,
+                  onChange: (state) => setState(() => value3 = state.value),
+                  tileBuilder: (context, state) {
+                    return S2Tile.fromState(
+                      state,
+                      trailing: const Icon(Icons.arrow_drop_down),
+                      isTwoLine: true,
+                    );
+                  }),
+              TextButton(
+                  onPressed: () {
+                    showAlertDialog(context);
+                  },
+                  child: Text('Tiempooo'))
             ],
           ),
         ),
@@ -166,9 +162,56 @@ List<S2Choice<String>> options = [
   S2Choice<String>(value: 'c', title: 'Difícil'),
 ];
 
+String value2 = '0';
+List<S2Choice<String>> categorias = [
+  S2Choice<String>(value: 'a', title: 'Origami'),
+  S2Choice<String>(value: 'b', title: 'Lifehack'),
+  S2Choice<String>(value: 'c', title: 'Téxtil'),
+  S2Choice<String>(value: 'd', title: 'Jardineria'),
+  S2Choice<String>(value: 'e', title: 'Bricolaje'),
+  S2Choice<String>(value: 'f', title: 'Electrónica'),
+  S2Choice<String>(value: 'g', title: 'Pintura'),
+];
+
 List<int> value3 = [2];
 List<S2Choice<int>> frameworks = [
   S2Choice<int>(value: 1, title: 'Papel'),
   S2Choice<int>(value: 2, title: 'Piedra'),
   S2Choice<int>(value: 3, title: 'Tijeras'),
 ];
+
+showAlertDialog(BuildContext context) {
+  Widget cancelButton = TextButton(
+    child: Text("Sí"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+  Widget continueButton = TextButton(
+    child: Text("No"),
+    onPressed: () {
+      Navigator.pop(context);
+    },
+  );
+
+  Widget picker = TimePickerSpinner(
+    is24HourMode: true,
+    normalTextStyle: TextStyle(fontSize: 24, color: azul_logo),
+    highlightedTextStyle: TextStyle(fontSize: 24, color: azul_logo),
+    spacing: 50,
+    itemHeight: 80,
+    isForce2Digits: true,
+  );
+
+  AlertDialog alert = AlertDialog(
+    content: picker,
+    actions: [cancelButton],
+  );
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
