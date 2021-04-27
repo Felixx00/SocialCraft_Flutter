65180d2ce9c971a9c4059a3fd091677b2daa7711
iamfelixf <felix.forroll@gmail.com>
2021-04-27 10:37:41 +0200

    fixv2 foto perfil

--- lib/upload.dart
+++ lib/upload.dart
@@ -148,7 +148,6 @@ class UploadState extends State<Upload> {
                   ),
                   onChanged: (newValue) {
                     desc = newValue;
-                    correct = true;
                     setState(() {});
                   },
                 ).paddingLeft(10),
