488ea631f25a877e8fad003dcb08f16f2ba996f5
marti.westermeyer <marti.westermeyer@estudiantat.upc.edu>
2021-05-13 16:43:22 +0100

    home

--- lib/post_pasos.dart
+++ lib/post_pasos.dart
@@ -9,25 +9,8 @@ class PostPasos extends StatelessWidget {
   final String descripcion;
   final List<dynamic> pasos;
   final String tutId;
-  var linkfoto = "";
 
   PostPasos(this.titulo, this.descripcion, this.pasos, this.tutId);
-
-  Future getImage() async {
-    var r = FirebaseStorage.instance.ref('Posts/' + tutId + '/paso1');
-    try {
-      await r.getDownloadURL();
-      var ref =
-          FirebaseStorage.instance.ref().child('Posts/' + tutId + '/paso1');
-      linkfoto = (await ref.getDownloadURL()).toString();
-    } catch (err) {
-      var ref = FirebaseStorage.instance
-          .ref()
-          .child('Usuario_Default/default-user-image.png');
-      linkfoto = (await ref.getDownloadURL()).toString();
-    }
-    print(linkfoto);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +26,7 @@ class PostPasos extends StatelessWidget {
           children: List.generate(
             pasos.length,
             (int index) {
+              if (pasos[index]['Foto'] == null) pasos[index]['Foto'] = "";
               final pasoText = pasos[index]['Texto'];
               return Column(
                 children: [
@@ -52,9 +36,15 @@ class PostPasos extends StatelessWidget {
                             fontSize: 25,
                             color: azul_logo,
                             fontWeight: FontWeight.bold)),
-                    title: Image.network('https://picsum.photos/250?image=9'),
-                    subtitle: Text(pasoText,
-                        style: TextStyle(fontSize: 16, color: Colors.black)),
+                    title: Text(pasoText,
+                            style: TextStyle(fontSize: 16, color: Colors.black))
+                        .paddingOnly(bottom: 15, top: 15),
+                    subtitle: Container(
+                      height: pasos[index]['Foto'] != "" ? 300.0 : 0.0,
+                      width: 300.0,
+                      child: Image.network(pasos[index]['Foto'])
+                          .paddingOnly(bottom: 10),
+                    ),
                   ),
                   Divider(
                     height: 20,
