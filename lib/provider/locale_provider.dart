ef86ad4cd9645767fb634d0f5bf3bb176d20bdef
marti-oss <marti.serra.aguilera@gmail.com>
2021-06-03 12:56:11 +0200

    No tocar settings

--- lib/provider/locale_provider.dart
+++ lib/provider/locale_provider.dart
@@ -4,7 +4,22 @@ import 'package:socialcraft/l10n/l10n.dart';
 class LocaleProvider extends ChangeNotifier {
   Locale _localee;
   Locale get localee => _localee;
-  void setLocale(Locale localee){
+  void setLocale(String l){
+    Locale localee;
+    switch (l) {
+      case 'es':
+        localee = Locale('es');
+        break;
+      case 'ca':
+        localee = Locale('ca');
+        break;
+      case 'en':
+        localee = Locale('en');
+        break;
+      default:
+        localee = Locale('en');
+        break;
+    }
     if (!L10n.all.contains(localee)) return;
     _localee = localee;
     notifyListeners();
